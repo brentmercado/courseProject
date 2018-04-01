03bc947e7de143e57f0d367f97fe0c7f7c021145
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-04-01 11:30:47 -0700

    Display the list of recipes
    Uses ngFor to display the recipes from the recipes array in the RecipeListComponent. Buttons and achor tags do not work. Recipes array is static for now.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-   recipes = [];
+   recipes: Recipe[] = [
+      new Recipe("A Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
+      new Recipe("A Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
+   ];
 
   constructor() { }
 
