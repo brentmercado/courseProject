725e5358b729cc3141c7c85c5ca0285568b3cff2
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-04-16 20:20:45 -0700

    Create cross-component communication between RecipeItemComponent and RecipesComponent
    In the RecipeService, added an EventEmitter to emit data from components. The RecipeItemComponent uses this EventEmitter to emit the selected recipe to the RecipesComponent. The RecipesComponent receives this data through the subscribe method.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+   recipeSelected = new EventEmitter<Recipe>();
+
    private recipes: Recipe[] = [
       new Recipe("A Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
       new Recipe("Another Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
