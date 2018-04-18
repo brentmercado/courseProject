5bb1c288a145511307b5251a0a1077bb51ec7a50
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-04-17 22:04:00 -0700

    Display ingredients for each recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
    recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-      new Recipe("A Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
-      new Recipe("Another Test Recipe", "This is simply a test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
+      new Recipe(
+         "Tasty Schnitzel",
+         "A super-tasty Schnitzel - just awesome!",
+         "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+         [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+         ]
+      ),
+      new Recipe(
+         "Big Fat Burger",
+         "What else you need to say?",
+         "https://www.redrobin.com/content/dam/web/menu/2015-june/gourmet-cheeseburger-1100.jpg",
+         [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+         ]
+      )
    ];
 
    getRecipes() {
